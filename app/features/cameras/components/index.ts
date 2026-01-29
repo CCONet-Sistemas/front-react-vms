af3b8e72cd95918c5d3dccf34ccb151310d22f3b
Edenilson Dias dos Santos <edenilson.santos@cconet.com.br>
2026-01-29 11:53:46 -0300

    feature: ajustes de listagem de camera

--- app/features/cameras/components/index.ts
+++ app/features/cameras/components/index.ts
@@ -1,2 +1,3 @@
-export { CameraCard } from './CameraCard';
-export { CameraGrid } from './CameraGrid';
+export { CameraCard, cameraCardVariants, type CameraCardProps } from './CameraCard';
+export { CameraFilters, type CameraFilters as CameraFiltersType, type ViewMode } from './CameraFilters';
+export { CameraList } from './CameraList';
